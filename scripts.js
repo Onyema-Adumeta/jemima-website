12469fdcfa12070358d300a894636c40139143b9
Onyema Davidson Adumeta <badumeta@gmail.com>
2026-03-06 19:21:42 -0800

    Fix formatting and add transition effect to cards

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   cards.forEach((item, index) => {
     item.style.opacity = "0";
     item.style.transform = "translateY(20px)";
+
     setTimeout(() => {
       item.style.transition = "all 0.5s ease";
       item.style.opacity = "1";
